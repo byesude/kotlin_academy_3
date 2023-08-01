9305706ade8363969465d52859ecc81202e52aee
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-01 20:54:38 +0300

    Add files via upload

---
+++ soru4/Main.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinuygulamaseansi.uygulamaseansi3_01082023.soru4
+
+/*
+Bir listedeki elemanları tersine çeviren bir fonksiyon yazın.
+ */
+fun main() {
+    val liste = arrayListOf(1, 2, 3, 4, 5)
+    liste.reverse()
+    println("Ters çevrilmiş liste: $liste")
+}
