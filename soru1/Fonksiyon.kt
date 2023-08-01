ddc435de33711a86dd24bf1b28528b7027fc8eef
Ecem Sude Ozan <137602264+byesude@users.noreply.github.com>
2023-08-01 20:47:42 +0300

    Delete soru1 directory

--- soru1/Fonksiyon.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinuygulamaseansi.uygulamaseansi3_01082023.soru1
-
-fun listeElemanlari(arrayList: ArrayList<Int>): Int {
-    var toplam = 0
-    for (i in arrayList) {
-        toplam += i
-    }
-    return toplam
-}
